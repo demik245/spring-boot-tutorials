bc101688b483362f449f9458bca2dfa3d81a812d
demik245 <damianmar17@outlook.com>
2024-12-15 18:02:30 +0100

    graszfortniajtdawaja1v1fortajaant

--- 06-spring-boot-mvc/validationdemo/src/main/java/com/demik/springdemo/mvc/Customer.java
+++ 06-spring-boot-mvc/validationdemo/src/main/java/com/demik/springdemo/mvc/Customer.java
@@ -1,4 +1,69 @@
 package com.demik.springdemo.mvc;
 
+import com.demik.springdemo.mvc.validation.CourseCode;
+import jakarta.validation.constraints.*;
+
 public class Customer {
+
+    @CourseCode(value = "DEM", message = "must start with DEM")
+    private String courseCode;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    private String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less or equal to 10")
+    private Integer freePasses;
+
+    public @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits") String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits") String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less or equal to 10")
+
+
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(@Min(value = 0, message = "must be greater than or equal to zero") @Max(value = 10, message = "must be less or equal to 10") Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public @NotNull(message = "is required") @Size(min = 1, message = "is required") String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@NotNull(message = "is required") @Size(min = 1, message = "is required") String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 }
